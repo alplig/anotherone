8080fe08d1ae8eedc0e01a8574ae513438ad16bb
Alexey Platonov <alexey.platonov@magenta-technology.com>
2023-09-11 17:31:44 +0400

    feat: add liquibase

--- src/main/java/org/anotherone/SimpleApplication.java
+++ src/main/java/org/anotherone/SimpleApplication.java
@@ -1,7 +1,12 @@
 package org.anotherone;
 
-public class SimpleApplication {
-    public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
+public class SimpleApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
     }
 }
